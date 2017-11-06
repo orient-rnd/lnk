d1d2e56d1e5c547b62e147dcd0d79be6d1880f64
Duc Pham <phduc95@gmail.com>
2017-11-06 15:43:23 +0700

    commit changes

--- LNK.AppServices.Lnk/Models/FlashCard/CreateFlashCardModel.cs
+++ LNK.AppServices.Lnk/Models/FlashCard/CreateFlashCardModel.cs
@@ -5,9 +5,23 @@ using System.Threading.Tasks;
 
 namespace LNK.AppServices.Lnk.Models.FlashCard
 {
-    public class CreateFlashCard
+    public class CreateFlashCardModel
     {
+        public string Id { get; set; }
 
-    
+        public string FaceA { get; set; }
+
+        public string FaceB { get; set; }
+
+        public string FlashCardCategoryId { get; set; }
+
+        public string FlashCardCategoryName { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public int ViewNumber { get; set; }
+
     }
 }
