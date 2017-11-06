0e4d4acc9cdc51479067531ef3350e4054a9bde4
DESKTOP-43ATN9B\Logan- <logan2805@gmail.com>
2017-11-06 14:51:45 +0700

    Loading page About us from file text 1.0

--- LNK.AppServices.Lnk/Controllers/HomeController.cs
+++ LNK.AppServices.Lnk/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LNK.AppServices.Lnk.Models.Home;
 
 namespace LNK.AppServices.Lnk.Controllers
 {
@@ -15,9 +16,25 @@ namespace LNK.AppServices.Lnk.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            var team = new List<Team>();
+            string path = @"E:\InkEnglish\LNK.AppServices.Lnk\wwwroot\images\profiles\profiles.txt";
+            var rawtext = System.IO.File.ReadAllText(path);
+            var dep = rawtext.Split('!');
+            foreach (var item in dep)
+            {
+                var itemTeam = new Team();
+                var person = item.Split(';');
+                itemTeam.Name = person[0];
+                itemTeam.Profiles = new List<Profile>();
+                for (int i = 1; i < person.Length; i++)
+                {
+                    var personInfo = person[i].Split('|');
+                    
+                    itemTeam.Profiles.Add(new Profile() { Name = personInfo[0], Position = personInfo[1], Time = personInfo[2], Img = personInfo[3] });                    
+                }
+                team.Add(itemTeam);
+            }
+            return View(team);
         }
 
         public IActionResult Contact()
