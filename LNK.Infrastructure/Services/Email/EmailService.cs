fa2c13c37c09041c1360262ed8e9cd200852708d
ORIENTSOFTWARE\nhloc <nguyenhuuloc304@gmail.com>
2017-11-06 15:30:18 +0700

    update email service

---
+++ LNK.Infrastructure/Services/Email/EmailService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace LNK.Infrastructure.Services.Email
+{
+    public class EmailService : IEmailService
+    {
+        public void SendMail(string email, string title, string massege)
+        {
+            SmtpClient client = new SmtpClient("mysmtpserver");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("username", "password");
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("whoever@me.com");
+            mailMessage.To.Add("receiver@me.com");
+            mailMessage.Body = "body";
+            mailMessage.Subject = "subject";
+            client.Send(mailMessage);
+        }
+    }
+}
